a3e87e1db18a109331d4d24f59d2a65bc5aae254
zhaozizhou <964175080@qq.com>
2021-01-13 10:26:53 +0800

    1

--- python-redis/test_redis.py
+++ python-redis/test_redis.py
@@ -8,19 +8,23 @@ from redis.sentinel import Sentinel
 dic_name_to_sentinel={}
 
 ###创建数据库连接
-#conn = pymysql.connect(host='192.168.1.10', port=3306, user='mozis', passwd='ktlshy34YU$',db='server_change',charset="utf8")
+#conn = pymysql.connect(host='192.168.1.6', port=3306, user='mozis', passwd='ktlshy34YU$',db='server_change',charset="utf8")
 #cursor = conn.cursor()
 # 使用 execute()  方法执行 SQL 查询 
-#cursor.execute("select name ,topology,data_ip,business_ip,service_name from redis_sentinel where topology = 'RedisMasterSlave';")
+#cursor.execute("select 1;")
 # 使用 fetchone() 方法获取单条数据.
 #data = cursor.fetchall()
 # 关闭数据库连接
 #conn.close()
 
-#取master0  mastername sentinek
+#取master0  mastername sentinel等信息
 def all_sentinel_get():
-    #conn = pymysql.connect(host='192.168.1.10', port=3306, user='mozis', passwd='ktlshy34YU$',db='server_change',charset="utf8")
-    #cursor = conn.cursor()
+    conn = pymysql.connect(host='192.168.1.6', port=3306, user='mozis', passwd='ktlshy34YU$',db='server_change',charset="utf8")
+    cursor = conn.cursor()
+    try:
+        cursor.execute("truncate table redis_sentinel;")
+    except:
+        return("table redis_sentinel not exist")
     r=redis.Redis(host='192.168.1.10',port=26379)
     #print(r.info(section=None)['master0']['name'])
     all=r.info(section='Sentinel')
@@ -35,12 +39,18 @@ def all_sentinel_get():
         #print(num_sentinel)
         status_sentinel=all[num]['status']
         slave_sentinel=all[num]['slaves']
-
-
+        try:
+            cursor.execute("insert into redis_sentinel values('{0}','{1}','{2}','{3}','{4}');".format(num,name_sentinel,status_sentinel,slave_sentinel,num_sentinel))
+            conn.commit()
+        except:
+            conn.rollback()
+    conn.close()
+    return("all get ok")
 
 
 def main():
-    all_sentinel_get()
+    sentinel_get=all_sentinel_get()
+    print(sentinel_get)
 
 
 
