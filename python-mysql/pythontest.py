cc684ebeb357cd5425c820be61b4baab7adb15e4
zhaozizhou <964175080@qq.com>
2021-01-19 18:26:00 +0800

    many

--- python-mysql/pythontest.py
+++ python-mysql/pythontest.py
@@ -14,22 +14,51 @@
 #print(list_pri[min:max])
 
 
-import pymysql
-list_pri=[]
-conn = pymysql.connect(host='192.168.1.6', port=3306, user='mozis', passwd='ktlshy34YU$',db='zzztest',charset="utf8")
-cursor = conn.cursor()
-cursor.execute("select id from sbtest10;")
-#count = cursor.fetchall()
-#count = cursor.fetchmany(5)
-while True:
-    count = cursor.fetchmany(2)
-    list_pri=[]
-    for i in count:
-        list_pri.append(i[0])
-    if count == ():
-        break
-    print(list_pri)
-tuple_list_pri=tuple(list_pri)
-#print(tuple_list_pri)
-conn.close()
+#import pymysql
+#list_pri=[]
+#conn = pymysql.connect(host='192.168.1.6', port=3306, user='mozis', passwd='ktlshy34YU$',db='zzztest',charset="utf8")
+#cursor = conn.cursor()
+#cursor.execute("select id from sbtest10;")
+##count = cursor.fetchall()
+##count = cursor.fetchmany(5)
+#while True:
+#    count = cursor.fetchmany(2)
+#    list_pri=[]
+#    for i in count:
+#        list_pri.append(i[0])
+#    if count == ():
+#        break
+#    print(list_pri)
+#tuple_list_pri=tuple(list_pri)
+##print(tuple_list_pri)
+#conn.close()
 
+import random
+import time
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+
+parallel=1
+def a(p):
+    p=p+1
+    print("a =={0}".format(p))
+    return(p)
+
+
+def b(d):
+    print("b =={0}".format(d))
+
+def print_error(value,):
+    print("error: ", value)
+
+if __name__=="__main__":
+    pool = Pool(processes=2)
+    for i in range(10):
+        zz=random.randrange(10)
+        print(zz)
+        pool.apply_async(func=a,args=(zz,),callback=b,error_callback=print_error)
+        #pool.apply_async(func=b,args=(q,),error_callback=print_error)
+        time.sleep(2)
+        #check_row_num(master_host,master_port,master_user,master_pass,master_database_name,slave_host,slave_port,slave_user,slave_pass,slave_database_name)
+    pool.close()
+    pool.join()
--- python-mysql/sql_md5.py
+++ python-mysql/sql_md5.py
@@ -3,20 +3,23 @@ import logging
 import os
 import hashlib
 #import itertools
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+
 
 #1.创建一个迭代器，拉取主键id。迭代主键ID 每次推进5w
 #2.二分法找到有问题的行
 #3.抓取列名到列表
 
-
+parallel=5
 master_host = '192.168.1.6'
 master_port = 3306
 master_user = 'mozis'
 master_pass = 'ktlshy34YU$'
 master_database_name = 'zzztest'
-tablename = 'sbtest10min'
+#tablename = 'sbtest10'
 #tablename = 'test50'
-slave_host = '192.168.1.6'
+slave_host = '192.168.21.128'
 slave_port = 3306
 slave_user = 'mozis'
 slave_pass = 'ktlshy34YU$'
@@ -39,9 +42,69 @@ debug.setFormatter(formatter_dbg)
 logger.addHandler(debug)
 
 
-def row_check(master_host,master_port,master_user,master_pass,master_database_name,tablename,columns):
+##表名
+def table_name_list():
+    list_table_name_tmp=[]
+    list_table_name=[]
+    try:
+        f = open("tablename.txt")             # 返回一个文件对象  
+    except IOError as e:
+        #print(e)
+        return(e)
+    else:
+        lines = f.readlines()
+        for line in lines:  
+            list_table_name_tmp.append(line)                 # 后面跟 ',' 将忽略换行符  
+        f.close()  
+        list_table_name=[x.strip() for x in list_table_name_tmp]
+        return(list_table_name)
+
+##取列名
+def column_name(master_host,master_port,master_user,master_pass,master_database_name,tablename):
+    conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
+    #conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
+    #cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor = conn.cursor()
+    cursor.execute("select COLUMN_NAME from information_schema.COLUMNS where TABLE_SCHEMA='{0}' and TABLE_NAME = '{1}';".format(master_database_name,tablename))
+    list_columns = cursor.fetchall()
+    columns=",".join('%s' %i for id in list_columns for i in id)
+    return(columns)
+
+def md5_check(master_database_name,tablename,pri,master_host,master_port,master_user,master_pass,columns,slave_database_name,slave_host,slave_port,slave_user,slave_pass,list_tmp1):
+    #print(list_pri)
     list_pri=[]
     list_error=[]
+    for i in list_tmp1:
+        list_pri.append(i[0])
+    buchang_in_pri=tuple(list_pri)
+    #print(buchang_in_pri)
+    master_md5=md5(master_database_name,tablename,pri,buchang_in_pri,master_host,master_port,master_user,master_pass,columns)
+    slave_md5=md5(slave_database_name,tablename,pri,buchang_in_pri,slave_host,slave_port,slave_user,slave_pass,columns)
+    #print(master_md5)
+    #print(slave_md5)
+    if master_md5 == slave_md5:
+        #print("{0} check ok!".format(buchang_in_pri))
+        logger.debug("TABLE {0} IS CHECKING".format(tablename))
+        #min+=1000
+    else:
+        list_error.append(buchang_in_pri)
+        logger.error("TABLE {0}  {1} check error!".format(tablename,buchang_in_pri))
+        print("TABLE {0}  {1} check error!".format(tablename,buchang_in_pri))
+    if len(list_error) == 0:
+        logger.info("TABLE {0} CHECK OK !".format(tablename))
+        #return()
+    else:
+        return(list_error)
+
+
+
+
+#遍历主键取xxxx行带入MD5
+def row_check(master_host,master_port,master_user,master_pass,master_database_name,tablename):
+    #pool = Pool(processes=10)
+    columns=column_name(master_host,master_port,master_user,master_pass,master_database_name,tablename)
+    #print(columns)
+    #list_pri=[]
     #dic_pri={}
     conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
     #cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -54,43 +117,40 @@ def row_check(master_host,master_port,master_user,master_pass,master_database_na
     #主键值
     cursor.execute("select {0} from {1} ".format(pri,tablename)) ########limit1
     #list_tmp1 = cursor.fetchall()  ###未处理的主键值
+    tpool = ThreadPool(10)# 创建一个线程池，20个线程数
     while True:
-        list_pri = []
-        list_tmp1 = cursor.fetchmany(1000)
-        #print(list_pri)
+        #list_pri = []
+        list_tmp1 = cursor.fetchmany(10)
         if list_tmp1 == ():
             break
-        for i in list_tmp1:
-            list_pri.append(i[0])
-        host=master_host
-        port=master_port
-        user=master_user
-        password=master_pass
-        database_name=master_database_name
-        buchang_in_pri=tuple(list_pri)
-        master_md5=md5(database_name,tablename,pri,buchang_in_pri,host,port,user,password,columns)
-        host=slave_host
-        port=slave_port
-        user=slave_user
-        password=slave_pass
-        database_name=slave_database_name
-        slave_md5=md5(database_name,tablename,pri,buchang_in_pri,host,port,user,password,columns)
-        #print(master_md5)
-        #print(slave_md5)
-        if master_md5 == slave_md5:
-            #print("{0} check ok!".format(buchang_in_pri))
-            logger.debug("{0} to {1} check ok!".format(min(buchang_in_pri),max(buchang_in_pri)))
-            #min+=1000
-        else:
-            list_error.append(buchang_in_pri)
-            print("{0} check error!".format(buchang_in_pri))
-            #min+=1000
+        #def md5_check():
+        #    #print(list_pri)
+        #    #for i in list_tmp1:
+        #        list_pri.append(i[0])
+        #    buchang_in_pri=tuple(list_pri)
+        #    print(buchang_in_pri)
+        #    master_md5=md5(master_database_name,tablename,pri,buchang_in_pri,master_host,master_port,master_user,master_pass,columns)
+        #    slave_md5=md5(slave_database_name,tablename,pri,buchang_in_pri,slave_host,slave_port,slave_user,slave_pass,columns)
+        #    #print(master_md5)
+        #    #print(slave_md5)
+        #    if master_md5 == slave_md5:
+        #        #print("{0} check ok!".format(buchang_in_pri))
+        #        logger.debug("TABLE {0} IS CHECKING".format(tablename))
+        #        #min+=1000
+        #    else:
+        #        list_error.append(buchang_in_pri)
+        #        logger.error("TABLE {0}  {1} check error!".format(tablename,buchang_in_pri))
+        #        print("TABLE {0}  {1} check error!".format(tablename,buchang_in_pri))
+        #        #min+=1000
+        #md5_check()
+        #tpool = ThreadPool(5)  # 创建一个线程池，20个线程数
+        #data_list = tpool.map(md5_check,callback=print_ture,error_callback=print_error) 
+        tpool.apply_async(func=md5_check,args=(master_database_name,tablename,pri,master_host,master_port,master_user,master_pass,columns,slave_database_name,slave_host,slave_port,slave_user,slave_pass,list_tmp1,),error_callback=print_error)
+    tpool.close()
+    tpool.join()
         #if list_tmp1 == ():
            #break
         #print(buchang_in_pri)
-    
-
-
     #print(type(list_num))
     #print(list_num)
     #pri = "".join('%s' %i for id in list_pri for i in id)
@@ -144,20 +204,37 @@ def row_check(master_host,master_port,master_user,master_pass,master_database_na
     #print(dic_pri)
     cursor.close() 
     conn.close()
-    return(list_error)
     #列名
+    return("{0} check done".format(tablename))
 
-def column_name(master_host,master_port,master_user,master_pass,master_database_name):
+#校验表名
+def check_table(master_database_name,tablename,master_host,master_port,master_user,master_pass,slave_database_name,slave_host,slave_port,slave_user,slave_pass):
     conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
-    #conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
     #cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor = conn.cursor()
-    cursor.execute("select COLUMN_NAME from information_schema.COLUMNS where TABLE_SCHEMA='{0}' and TABLE_NAME = '{1}';".format(master_database_name,tablename))
-    list_columns = cursor.fetchall()
-    columns=",".join('%s' %i for id in list_columns for i in id)
-    return(columns)
+    try:
+        # 执行sql语句
+        cursor.execute("select 1 from {0} limit 1;".format(tablename))
+    # except:
+    except pymysql.Error as e:
+        return(e.args[0], e.args[1])
+        # print(e[0])
+    conn = pymysql.connect(host=slave_host, port=int(slave_port), user=slave_user, passwd=slave_pass,db=slave_database_name)
+    #cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor = conn.cursor()
+    try:
+        # 执行sql语句
+        cursor.execute("select 1 from {0} limit 1;".format(tablename))
+    # except:
+    except pymysql.Error as e:
+        return(e.args[0], e.args[1])
+        # print(e[0])
+    row_check_tmp=row_check(master_host,master_port,master_user,master_pass,master_database_name,tablename)
+    return(row_check_tmp)
 
 
+
+#计算md5
 def md5(database_name,tablename,pri,buchang_in_pri,host,port,user,password,columns):
     conn = pymysql.connect(host=host, port=int(port), user=user, passwd=password,db=database_name)
     #conn = pymysql.connect(host=master_host, port=int(master_port), user=master_user, passwd=master_pass,db=master_database_name)
@@ -181,13 +258,28 @@ def md5(database_name,tablename,pri,buchang_in_pri,host,port,user,password,colum
     conn.close()
     return(row_md5)
 
+def print_error(value,):
+    print("error: ", value)
 
+def print_ture(value,):
+    print(value)
 
 def main():
-    columns=column_name(master_host,master_port,master_user,master_pass,master_database_name)
+    table_names=table_name_list()
+    print(table_names)
+    pool = Pool(processes=parallel)
+    for tablename in table_names:
+        pool.apply_async(func=check_table,args=(master_database_name,tablename,master_host,master_port,master_user,master_pass,slave_database_name,slave_host,slave_port,slave_user,slave_pass,),callback=print_ture,error_callback=print_error)
+            #pool.apply_async(func=row_check,args=(master_host,master_port,master_user,master_pass,master_database_name,tablename,),callback=print_ture,error_callback=print_error)
+    #check_row_num(master_host,master_port,master_user,master_pass,master_database_name,slave_host,slave_port,slave_user,slave_pass,slave_database_name)
+    pool.close()
+    pool.join()
+
+
+    #columns=column_name(master_host,master_port,master_user,master_pass,master_database_name,tablename)
     #print(columns)
-    error=row_check(master_host,master_port,master_user,master_pass,master_database_name,tablename,columns)
-    print(error)
+    #error=row_check(master_host,master_port,master_user,master_pass,master_database_name,tablename,)
+    #print(error)
 
 
 if __name__ == "__main__":
